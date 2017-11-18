36d5b4c32499c85f2e2a4182de120535f331c5eb
David <dagaydevel@free.fr>
2017-11-18 14:17:07 +0100

    ajout de la conf pour boomerang

---
+++ source/conf-boomerang.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+email = 'dagaydevel@free.fr'
+
+highlight_root_package = 'fr.maif'
+
+html_theme_options = {
+    'logo': 'logo-BCtraining Groupe.jpg',
+    'logo_name': True,
+    'logo_text_align': 'center',
+}
