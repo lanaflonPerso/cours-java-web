e1cab50baee1775e17dd541be00e9bb4492233ed
David <dagaydevel@free.fr>
2018-04-28 02:52:38 +0200

    refacto des confs de publication

--- source/conf-epsi-b3.py
+++ source/conf-epsi-b3.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+project = "Java / Java EE"
+
 email = 'david.gayerie.epsi@mailoo.org'
 
 highlight_root_package = 'fr.epsi.b3'
 
 todo_include_todos = False
 
+chapters=['langage_java', 'javaee_web', 'javaee_orm']
--- source/conf-epsi-i4.py
+++ source/conf-epsi-i4.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-project = "Java / Java EE"
+project = "Les API Web"
 
 email = 'david.gayerie.epsi@mailoo.org'
 
-highlight_root_package = 'com.cgi.udev'
+highlight_root_package = 'fr.epsi.i4'
 
 todo_include_todos = False
 
-chapters=['langage_java', 'javaee_web']
-
+chapters=['http']
