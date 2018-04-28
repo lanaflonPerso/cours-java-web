887f1bf709c56b4ce745413fe2fe66b091af04e6
David <dagaydevel@free.fr>
2018-04-28 23:40:29 +0200

    conf support pour POEI CGI 2018

---
+++ source/conf-poei-cgi-2018.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+project = "Java / Java EE"
+
+email = 'david.gayerie.epsi@mailoo.org'
+
+highlight_root_package = 'com.cgi.poei'
+
+todo_include_todos = False
+
+chapters=['uml', 'langage_java', 'javaee_web', 'javaee_orm', 'http']
+
