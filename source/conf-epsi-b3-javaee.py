d31c9b1bbe1e8702bd43d3f5220a22f0867d2654
David <dagaydevel@free.fr>
2018-05-01 15:46:08 +0200

    conf publication EPSI B3 Java EE

---
+++ source/conf-epsi-b3-javaee.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+project = "Java EE pour le Web"
+
+email = 'david.gayerie.epsi@mailoo.org'
+
+highlight_root_package = 'fr.epsi.b3'
+
+todo_include_todos = False
+
+chapters=['javaee_web']
