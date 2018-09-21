b94012472c5820bc9ab98beddb89c45aacd4424f
Nicolas Herbaut <nicolas.herbaut@gmail.com>
2018-09-21 17:37:18 +0200

    L2MIASH config

---
+++ source/conf-L2MIASHS.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+project = "Le langage Java"
+
+email = 'nicolas.herbaut@univ-paris1.fr'
+author='David Gayerie & Nicolas Herbaut'
+highlight_root_package = 'fr.univ_paris1.miashs'
+root_package_name='fr.univ_paris1.miashs'
+todo_include_todos = False
+
+chapters=['langage_java']
+
+tags=[
+"skip_package",
+
+]
